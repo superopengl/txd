49e00cf98b09808028528400f95e7a07b5377d86
Jun Shao <mr.shaojun@gmail.com>
2020-05-26 16:30:41 +1000

    feat: simple backdoor page

--- ubc-web/src/pages/BackdoorPage/BackdoorPage.js
+++ ubc-web/src/pages/BackdoorPage/BackdoorPage.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import ImageCardAdminTable from 'components/ImageCardAdminTable/ImageCardAdminTable';
+import { Tabs } from 'antd';
+const { TabPane } = Tabs;
 
+const ContainerStyled = styled.div`
+  margin: 2rem;
+`;
+
+function callback(key) {
+  console.log(key);
+}
 
 const BackdoorPage = () => (
-  <div>
-    BackdoorPage Component
-  </div>
+  <ContainerStyled>
+    <Tabs defaultActiveKey="1" onChange={callback}>
+      <TabPane tab="Poster" key="poster">
+        <ImageCardAdminTable></ImageCardAdminTable>
+      </TabPane>
+      <TabPane tab="Business" key="business">
+        Content of Tab Pane 2
+    </TabPane>
+      <TabPane tab="Event" key="event">
+        Content of Tab Pane 3
+    </TabPane>
+      <TabPane tab="Gallery" key="gallery">
+        Content of Tab Pane 3
+    </TabPane>
+    </Tabs>
+  </ContainerStyled>
 );
 
 BackdoorPage.propTypes = {};
