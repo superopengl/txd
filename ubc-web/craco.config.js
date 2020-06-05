6cc23df445bb98608d527f556b308c1f82eb62a7
Jun Shao <mr.shaojun@gmail.com>
2020-06-05 16:57:34 +1000

    fix build error

--- ubc-web/craco.config.js
+++ ubc-web/craco.config.js
@@ -6,16 +6,18 @@ module.exports = {
       plugin: CracoLessPlugin,
       options: {
         lessLoaderOptions: {
-          modifyVars: {
-            '@primary-color': '#81a9de',
-            '@font-size-base': '16px',
-            '@height-base': '40px',
-            '@height-lg': '48px',
-            '@height-sm': '32px',
-            '@border-radius-base': '4px',
-          },
-          javascriptEnabled: true,
-        },
+          lessOptions: {
+            modifyVars: {
+              '@primary-color': '#81a9de',
+              '@font-size-base': '16px',
+              '@height-base': '40px',
+              '@height-lg': '48px',
+              '@height-sm': '32px',
+              '@border-radius-base': '4px',
+            },
+            javascriptEnabled: true,
+          }
+        }
       },
     },
   ],
