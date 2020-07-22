e796f57b6e022b99d3820852fcb44fc781a811fc
Jun Shao <mr.shaojun@gmail.com>
2020-07-22 11:13:27 +1000

    fix compile error

--- txd-web/craco.config.js
+++ txd-web/craco.config.js
@@ -6,14 +6,16 @@ module.exports = {
       plugin: CracoLessPlugin,
       options: {
         lessLoaderOptions: {
-          javascriptEnabled: true,
-          modifyVars: {
-            '@primary-color': '#22075e',
-            '@font-size-base': '16px',
-            '@height-base': '40px',
-            '@height-lg': '48px',
-            '@height-sm': '32px',
-            '@border-radius-base': '4px',
+          lessOptions: {
+            javascriptEnabled: true,
+            modifyVars: {
+              '@primary-color': '#22075e',
+              '@font-size-base': '16px',
+              '@height-base': '40px',
+              '@height-lg': '48px',
+              '@height-sm': '32px',
+              '@border-radius-base': '4px',
+            }
           }
         }
       },
