b425221e61d84045b456bc5a021c32c3d5bf1e6b
Jun Shao <mr.shaojun@gmail.com>
2020-08-07 18:21:33 +1000

    Clean up dep

--- txd-web/craco.config.js
+++ txd-web/craco.config.js
@@ -6,17 +6,26 @@ module.exports = {
       plugin: CracoLessPlugin,
       options: {
         lessLoaderOptions: {
-          lessOptions: {
-            javascriptEnabled: true,
-            modifyVars: {
-              '@primary-color': '#22075e',
-              '@font-size-base': '16px',
-              '@height-base': '40px',
-              '@height-lg': '48px',
-              '@height-sm': '32px',
-              '@border-radius-base': '4px',
-            }
-          }
+          javascriptEnabled: true,
+          modifyVars: {
+            '@primary-color': '#22075e',
+            '@font-size-base': '16px',
+            '@height-base': '40px',
+            '@height-lg': '48px',
+            '@height-sm': '32px',
+            '@border-radius-base': '4px',
+          },
+          // lessOptions: {
+          //   javascriptEnabled: true,
+          //   modifyVars: {
+          //     '@primary-color': '#22075e',
+          //     '@font-size-base': '16px',
+          //     '@height-base': '40px',
+          //     '@height-lg': '48px',
+          //     '@height-sm': '32px',
+          //     '@border-radius-base': '4px',
+          //   }
+          // }
         }
       },
     },
