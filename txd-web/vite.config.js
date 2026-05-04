9d15b50af1299e34ce127f2bef992ffd5317c134
Jun Shao <mr.shaojun@gmail.com>
2026-05-04 13:26:31 +1000

    clean up

--- txd-web/vite.config.js
+++ txd-web/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
 import path from 'path';
 
 export default defineConfig({
   plugins: [
     react(),
-    svgr(),
   ],
   resolve: {
     alias: {
